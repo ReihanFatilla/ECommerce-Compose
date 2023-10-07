a178d80b8cb79afbe907df3bac2ea8f738a87fe2
ReihanFatilla <reihanfatilla@gmail.com>
2023-10-07 12:36:09 +0700

    [feat]: added buildSrc folder on root and build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    implementation(project(":core"))
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
