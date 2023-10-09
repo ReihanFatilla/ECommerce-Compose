d750ede3de346991c099181c23c99c419f7bb183
ReihanFatilla <reihanfatilla@gmail.com>
2023-10-09 21:09:59 +0700

    [feat]: improved navigation animation

--- app/src/main/java/com/trawlbens/reift/e_commerce/MainActivity.kt
+++ app/src/main/java/com/trawlbens/reift/e_commerce/MainActivity.kt
@@ -3,11 +3,13 @@ package com.trawlbens.reift.e_commerce
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandHorizontally
 import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.animation.shrinkOut
+import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +18,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
@@ -46,11 +50,19 @@ class MainActivity : ComponentActivity() {
                     val navHostEngine = rememberAnimatedNavHostEngine(
                         navHostContentAlignment = Alignment.TopCenter,
                         rootDefaultAnimations = RootNavGraphDefaultAnimations(
-                            enterTransition = { slideInHorizontally(animationSpec = tween(500)) { it } },
-                            exitTransition = {
-                                shrinkHorizontally(
+                            enterTransition = { slideInHorizontally(animationSpec = tween(500), initialOffsetX = { it }) },
+                            exitTransition = { slideOutHorizontally(animationSpec = tween(500)) },
+                            popExitTransition = {
+                                slideOutOfContainer(
                                     animationSpec = tween(500),
-                                    shrinkTowards = Alignment.Start
+                                    towards = AnimatedContentScope.SlideDirection.End
+                                )
+                            },
+                            popEnterTransition = {
+                                slideIntoContainer(
+                                    animationSpec = tween(500),
+                                    towards = AnimatedContentScope.SlideDirection.End,
+                                    initialOffset = { it/2 }
                                 )
                             }
                         )
